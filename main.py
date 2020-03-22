5db81448a823b2f1852f5a6a43dce4375204e972
Guilherme Bacellar <guibacellar@gmail.com>
2020-03-22 11:22:42 -0300

    Updated Feature Generation Notebook + Added Notebook Execution on main.py
    Signed-off-by: Guilherme Bacellar <guibacellar@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Download, Parse, Generate Features and Save by CountryCode
 
+import os
 import cv19_downloader
 import cv19_parser
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
-import os
+
 
 def main() -> None:
 
     # Download and Parse Data
-    # if cv19_downloader.download_timeline_data():
-    #      cv19_parser.parse_timeline_raw_data_to_csv()
+    if cv19_downloader.download_timeline_data():
+        cv19_parser.parse_timeline_raw_data_to_csv()
 
     # Run Feature Generation Notebook
     print('Running Feature Generation Notebook: ', end='')
@@ -18,6 +19,7 @@ def main() -> None:
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
     ep.preprocess(nb, {'metadata': {'path': os.path.abspath(os.getcwd())}})
     print('DONE')
+
 
 if __name__ == '__main__':
     main()
