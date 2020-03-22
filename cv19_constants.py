1b60dab04975b3db958bec419b081d34fe5a8dac
Guilherme Bacellar <guibacellar@gmail.com>
2020-03-22 11:22:39 -0300

    Updated Feature Generation Notebook + Added Notebook Execution on main.py
    Signed-off-by: Guilherme Bacellar <guibacellar@gmail.com>

--- cv19_constants.py
+++ cv19_constants.py
@@ -3,3 +3,4 @@
 TARGET_API: str = 'https://thevirustracker.com/timeline/map-data.json'
 TARGET_TIMELINE_RAW_DATA: str = 'data/all_timeline.json'
 TARGET_TIMELINE_CSV_DATA: str = 'data/all_timeline.csv'
+TARGET_TIMELINE_FEATURED_CSV_DATA: str = 'data/all_timeline_with_features.csv'
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import cv19_constants
+# Download, Parse, Generate Features and Save by CountryCode
+
 import cv19_downloader
 import cv19_parser
-
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import os
 
 def main() -> None:
 
     # Download and Parse Data
-    if cv19_downloader.download_timeline_data():
-         cv19_parser.parse_timeline_raw_data_to_csv()
+    # if cv19_downloader.download_timeline_data():
+    #      cv19_parser.parse_timeline_raw_data_to_csv()
+
+    # Run Feature Generation Notebook
+    print('Running Feature Generation Notebook: ', end='')
+    nb = nbformat.read(open('feature_generation.ipynb'), as_version=4)
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb, {'metadata': {'path': os.path.abspath(os.getcwd())}})
+    print('DONE')
 
 if __name__ == '__main__':
     main()
